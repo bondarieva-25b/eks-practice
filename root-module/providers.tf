a9c3b1f98ff965367c7fac8f4cb6db4701b898b9
bondarieva-25b <nadiia.bon@edu.312school.com>
2026-01-21 22:56:07 -0600

    test tf plan in staging with correct tfstate bucket

--- root-module/providers.tf
+++ root-module/providers.tf
@@ -7,8 +7,8 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "practice-eks-450558842433-tfstate"
-    #bucket = "practice-eks-058316962389-tfstate-staging"
+    #bucket = "practice-eks-450558842433-tfstate"
+    bucket = "practice-eks-058316962389-tfstate-staging"
     key    = "terraform.tfstate"
     region = "us-east-1"
     use_lockfile = true
